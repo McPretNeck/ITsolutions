382d84adae355906ebb829edc9129e1a9b711d4e
McPretNeck <r.e.heeren@st.hanze.nl>
2019-01-09 15:55:43 +0100

    online db ingesteld

--- db.php
+++ db.php
@@ -1,8 +1,16 @@
 <?php
+//old
 $dbhost="127.0.0.1";
 $dbuser="root";
 $dbpass="";
 $dbname="its";
+
+//new online
+$dbhost="sql7.freemysqlhosting.net";
+$dbuser="sql7273416";
+$dbpass="jZ2kkDeeJ9";
+$dbname="sql7273416";
+
 $db= mysqli_connect($dbhost,$dbuser,$dbpass,$dbname);
 //test of verbinding werkt:
 if(mysqli_connect_errno()){
