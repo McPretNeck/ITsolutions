b26783b76d193e39f27bb22be9b64a7c105e689a
Robert Heeren <r.e.heeren@st.hanze.nl>
2019-01-10 15:54:18 +0100

    Formulier voor toevoegen leveranciers

--- test.php
+++ test.php
@@ -1,82 +1,78 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<title>Formulier</title>
-	<style>
-		label {display:block;}
-		:invalid {background: #ffdddd;}
-	</style>
-</head>
-
-<body>
-	<form>
-		<fieldset>
-		
-			<legend>Contactgegevens</legend>
-		
-			<label for="name">Naam *</label>
-			<input type="text" id="name" required>
-
-			<label for="email">Email *</label>
-			<input type="email" id="email" required>
-
-			<label for="straat">Straatnaam</label>
-			<input type="text" id="straat" placeholder="Zonder huis nummer">
-
-			<label for="nummer">Huis nummer</label>
-			<input type="number" id="nummer">
-
-			<label for="postcode">Postcode</label>
-			<input type="number" class="postcode" pattern="[0-9]{4}" placeholder="4 nummers"/> 
-			<input type="text" class="postcode" pattern="[A-Z]{2}" placeholder="2 letters"/>
-
-			<label for="land">Land</label>
-			<input type="text" id="land">
-		
-			<label for="telefoon">Telefoon</label>
-			<input type="tel" id="telefoon">
-		
-		</fieldset>
-
-		<fieldset>
-	
-			<legend>Voorkeuren</legend>
-		
-			<label for="kleur">Lievelings kleur</label>
-			<input type="color" id="kleur">
-		
-			<label for="maand">Leukste maand</label>
-			<select id="maand">
-				<option>januari</option>
-				<option>februari</option>
-				<option>maart</option>
-				<option>april</option>
-				<option>mei</option>
-				<option>juni</option>
-				<option>juli</option>
-				<option>augustus</option>
-				<option>september</option>
-				<option>oktober</option>
-				<option>november</option>
-				<option>december"</option>
-			</select>
-		
-			<label for="cijfer">Hoe goed is dit ongeveer?</label>
-			<select id="maand">
-				<option>1</option>
-				<option>2</option>
-				<option>3</option>
-				<option>4</option>
-				<option>5</option>
-				<option>6</option>
-				<option>7</option>
-				<option>8</option>
-				<option>9</option>
-				<option>10</option>
-			</select>
-		</fieldset>
-		<input type="submit" value="Submit" />
-		
+<?php
+include 'menu.php';
+include 'db.php';
+?>
+<div class="container mt-2">
+	<form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="post">
+		<table frame="box" class="ml-auto mr-auto" style="text-align:right;">
+		<tbody>
+			<tr><td colspan="2" style="text-align:center;">
+				<legend>Leveranier gegevens</legend>
+			</tr></td>
+			<tr>
+				<td>
+					<label for="name">Naam<span style="color:Red;">*</span></label>
+				</td><td>
+					<input type="text" id="name" name="naam" required> 
+				</td>
+			</tr>
+			<tr>
+				<td>
+					<label for="email">Email<span style="color:Red;">*</span></label>
+				</td><td>	
+					<input type="email" name="email" id="email" pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}" required>
+				</td>
+			</tr>
+			<tr>
+				<td>
+					<label for="straat">Straatnaam</label>
+				</td><td>	
+					<input type="text" name="straat" id="straat" placeholder="Zonder huis nummer">
+				</td>
+			</tr>
+			<tr>
+				<td>
+					<label for="huisnummer">Huis nummer<span style="color:Red;">*</span></label>
+				</td><td>	
+					<input type="number" name="huisnummer" id="huisnummer" required>
+				</td>
+			</tr>
+			<tr>
+				<td>
+					<label for="toevoeging">Toevoeging</label>
+				</td><td>	
+					<input type="text" name="toevoeging" id="toevoeging">
+				</td>
+			</tr>
+			<tr>
+				<td>
+					<label for="postcode">Postcode<span style="color:Red;">*</span></label>
+				</td><td>	
+					<input type="text" name="postcode" class="postcode" pattern="[0-9]{4}[A-Z][{2}" placeholder="1234AB" maxlength="6"  required>
+				</td>
+			</tr>
+			<tr>
+				<td>
+					<label for="land">Land</label>
+				</td><td>	
+					<input type="text" name="land" id="land">
+				</td>
+			</tr>
+			<tr>
+				<td>
+					<label for="telefoon">Telefoon<span style="color:Red;">*</span></label>
+				</td><td>	
+					<input type="tel" name="telefoon" id="telefoon" pattern="[0-9]{10}" placeholder="0612345678" maxlength="10" required>
+				</td>
+			</tr>
+			<tr>
+				<td></td>
+				<td style="text-align:left; align:left;">
+					<input type="submit" value="Toevoegen" />
+				</td>
+			</tr>
+		</tbody>
+		</table>
 	</form>
-</body>
+</div>
 </html>
