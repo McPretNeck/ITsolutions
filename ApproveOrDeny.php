ceffbfcfe09a6e164da1f6a823932e0470541f57
McPretNeck <r.e.heeren@st.hanze.nl>
2019-01-29 13:54:53 +0100

    form margiet

--- ApproveOrDeny.php
+++ ApproveOrDeny.php
@@ -3,8 +3,8 @@ include 'menu.php';
 include 'db.php';
 
 //functie voor het verkrijgen van alle producten
-	//moet nog een manier komen om de bestellings id in de query te plaatsen
-	$query = "SELECT `bestelling`.`BestellingID`, `producten`.`ProductID` as 'ID', `bestelling`.`GebruikerID`, `bestelling`.`Status`, `bestelling`.`Reden`, `bestelling`.`Opmerking`, `producten`.`Naam`, `producten`.`Prijs` FROM `bestelling` JOIN `productenbesteld` on `bestelling`.`BestellingID` = `productenbesteld`.`BestellingID` JOIN `producten` on `productenbesteld`.`ProductID` = `producten`.`ProductID` where `bestelling`.`BestellingID`= 1;";
+	//Query die selecteerd op of het product over de 500 euro is of niet.
+	$query = "SELECT `bestelling`.`BestellingID`, `producten`.`ProductID` as 'ID', `bestelling`.`GebruikerID`, `bestelling`.`Status`, `bestelling`.`Reden`, `bestelling`.`Opmerking`, `producten`.`Naam`, `producten`.`Prijs` FROM `bestelling` JOIN `productenbesteld` on `bestelling`.`BestellingID` = `productenbesteld`.`BestellingID` JOIN `producten` on `productenbesteld`.`ProductID` = `producten`.`ProductID` WHERE `Prijs` > 500 ORDER BY `BestellingID`";
 	$result = mysqli_query($db, $query);
 	$text ="";
 	
@@ -19,9 +19,9 @@ include 'db.php';
 	$p = $row['Status'];
 	$o = $row['Opmerking'];
 	
-	$text .= "
+	$text = "
 	<form method=post action=ApproveOrDeny.php>
-	<table class=\"shadow mx-auto mt-3\" style=\"border:1px solid;\">
+	<table class=\"shadow mx-auto mt-2\" style=\"border:1px solid;\">
 	<span border=\"1\">
 	<tr>
 	<td>Bestelling ID : ".$bs." </br>Product ID: ".$ID." </td><td style=\"width:120px;\">
@@ -34,29 +34,39 @@ include 'db.php';
 	<td class=\"float-right text-right\" style=\"width:120px;\"><button type=submit name=accepteer>Accepteer</button></td></tr>
 	<tr><td colspan=\"3\">
 	</td></tr></span>
-	</table></form> ";
+	</table></form> 
+	<br/>
+	<div class=\"mb-5\"></div>";
 	
-	
-	}
 	echo $text;
-}
-	
-	
-	
-	if(isset($_POST["accepteer"])){
+
+	if(isset($_POST["afwijzen"])){
+		include 'db.php';
+	$query = "UPDATE `bestelling` SET `Status` = 'Denied' WHERE `BestellingID` = $bs";
+	if(mysqli_query($db,$query)){
 		echo "<p align=\"CENTER\">De order is afgewezen.</br>";
 		echo "De gegevens hierboven worden afgewezen</br>";
 		echo "</p>";
-		//hier
 	}
-	if(isset($_POST["afwijzen"])){
+	}
+
+	if(isset($_POST["accepteer"])){
+		include 'db.php';
+	$query = "UPDATE `bestelling` SET `Status` = 'Accepted' WHERE `BestellingID` = $bs";
+	if(mysqli_query($db,$query)){
 		echo "<p align=\"CENTER\">De order is geaccepteerd en word doorgestuurd.</br>";
 		echo "De gegevens hierboven zijn geaccepteerd</br>";
 		echo "</p>";
-		
-	include 'db.php';
-	$query = "UPDATE `bestelling` SET `Status` = 'Accepted' WHERE `$bs`";
 	}
+		
+	}
+}
+	
+}
+	
+	
+
+	
 
 
 ?>
